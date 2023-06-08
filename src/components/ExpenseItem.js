11f3c2846a9eb4ab6d5a40d83ab5d573e84cc455
delissgt <delissgt@gmail.com>
2023-06-08 17:30:55 -0500

    More Complex Component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+
+function ExpenseItem() {
+  return (
+      <div>
+        <div>Date today 8 June 2023</div>
+        <div>
+          <h2>New Car</h2>
+          <div>$100.50</div>
+        </div>
+      </div>
+  );
+}
+
+export default ExpenseItem;
