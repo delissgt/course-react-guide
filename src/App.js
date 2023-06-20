264bedc6977138e84ffe18bdb33de593092567b8
delissgt <delissgt@gmail.com>
2023-06-20 17:30:53 -0600

    Exercise create a home page using components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import Expenses from "./components/Expenses/Expenses";
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
 
-import './App.css';
+const concepts = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+  },
+  {
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+  },
+];
 
-
-const App = () => {
-
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Paper',
-      amount: 1,
-      date: new Date(2012, 2, 8),
-    },
-    {
-      id: 'e2',
-      title: 'Soup',
-      amount: 2,
-      date: new Date(2011, 3, 4),
-    },
-    {
-      id: 'e3',
-      title: 'Lolipops',
-      amount: 3,
-      date: new Date(2022, 5, 12),
-    },
-    {
-      id: 'e4',
-      title: 'Juice',
-      amount: 2,
-      date: new Date(2011, 9, 3),
-    }
-  ]
-
+function App() {
   return (
-    <div className="App">
-      <Expenses items={expenses} />
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
+      </header>
+      <ul id="concepts">
+        <li className="concept">
+          <img src="TODO: IMAGE" alt="TODO: TITLE" />
+          <h2>TODO: TITLE</h2>
+          <p>TODO: DESCRIPTION</p>
+        </li>
+      </ul>
     </div>
   );
 }
